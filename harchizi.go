0ca85565314b2f69b20892d475fc5918abd56bbf
Sohail Siadatnejad <sohale@gmail.com>
2018-11-09 23:43:03 +0330

    multiple channels <- (receive) ed

--- harchizi.go
+++ harchizi.go
@@ -20,15 +20,22 @@ func main() {
 	// make: slice (dynamic array), channels, maps.
 	//var ch chan string // nil
 	ch := make(chan string)
+	var ch2 chan string = make(chan string)
 	go reps("aabbbbbce122222", ch)
+	go func(s string, ch2 chan string) {
+		ch2 <- "hi " + s
+	}("joojooo", ch2)
+
 	// time.Sleep(100*time.Millisecond)
 	// NOT:     for s string in ch {
 
 	//for {
-	s := <-ch
+	s := <-ch2
 	fmt.Println(s)
 	//}
 	/*{
 	    fmt.Println(r, ch)
 	}*/
+	s = <-ch
+	fmt.Println(s)
 }
